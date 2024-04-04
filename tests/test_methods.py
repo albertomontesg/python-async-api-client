3c811fc74d2336e6a96ef7fcdc6044d4743ee254
Alberto Montes <alm59321@gmail.com>
2024-04-04 18:30:30 +0200

    Fix test naming

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from meteoswiss_async.hello_world import hello_world
 
 
-def hello_test():
+def test_hello():
     """
     This defines the expected usage, which can then be used in various test cases.
     Pytest will not execute this code directly, since the function does not contain the suffix "test"
